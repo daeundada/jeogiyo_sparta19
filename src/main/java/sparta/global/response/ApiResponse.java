2d7cc7f9d2f4e0b2aa61b775ca1064ce751b6438
de123456sdf <cdaeun95@gmail.com>
2024-08-23 14:18:15 +0900

    feat: 응답 설정

---
+++ src/main/java/sparta/global/response/ApiResponse.java
@@ -0,0 +1,21 @@
+package sparta.global.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse<T>{
+
+    private String message;
+    private T data;
+
+    public ApiResponse(String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(String message, T data) {
+        return new ApiResponse<>(message, data);
+    }
+}
