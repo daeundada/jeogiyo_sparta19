c6742f326c370d254110d749997214af19e9be04
de123456sdf <cdaeun95@gmail.com>
2024-08-23 14:18:49 +0900

    feat: 초기 환경 설정

---
+++ src/test/java/sparta/jeogiyo/JeogiyoApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.jeogiyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeogiyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
