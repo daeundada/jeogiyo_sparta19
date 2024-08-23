e5368628ecf7708408dc85e332c6335247c44452
de123456sdf <cdaeun95@gmail.com>
2024-08-23 14:18:34 +0900

    feat: JpaAuditing 설정

---
+++ src/main/java/sparta/jeogiyo/JeogiyoApplication.java
@@ -0,0 +1,16 @@
+package sparta.jeogiyo;
+
+import jakarta.persistence.Entity;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class JeogiyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeogiyoApplication.class, args);
+    }
+
+}
