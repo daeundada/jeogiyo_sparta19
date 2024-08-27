b22ca1bf65950841edd930cc534c7ea4987d7cff
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-08-27 10:13:22 +0900

    feat: User 관련 클래스들 추가

--- src/main/java/sparta/jeogiyo/domain/user/UserDetailsImpl.java
+++ src/main/java/sparta/jeogiyo/domain/user/UserDetailsImpl.java
@@ -1,24 +1,39 @@
 package sparta.jeogiyo.domain.user;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import sparta.jeogiyo.domain.user.entity.User;
 
 public class UserDetailsImpl implements UserDetails {
 
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return user.getUsername();
     }
+
+    public User getUser() {
+        return user;
+    }
+
 }
--- src/main/java/sparta/jeogiyo/domain/user/dto/request/UserUpdateRequestDto.java
+++ src/main/java/sparta/jeogiyo/domain/user/dto/request/UserUpdateRequestDto.java
@@ -1,8 +1,37 @@
 package sparta.jeogiyo.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserUpdateRequestDto {
+
+    @NotBlank(message = "닉네임은 필수 항목입니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,8}$", message = "닉네임은 특수문자를 제외한 2글자에서 8글자 사이여야 합니다.")
+    private String nickname;
+
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8자, 최대 15자 이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "비밀번호는 하나 이상의 대문자, 소문자, 숫자, 특수 문자를 포함해야 합니다."
+    )
+    private String currentPassword;
+
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8자, 최대 15자 이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "비밀번호는 하나 이상의 대문자, 소문자, 숫자, 특수 문자를 포함해야 합니다."
+    )
+    private String updatePassword;
+
+    @NotBlank(message = "주소는 필수 항목입니다.")
+    private String address;
+
+    public void setPassword (String password) {
+        this.updatePassword = password;
+    }
 
 }
