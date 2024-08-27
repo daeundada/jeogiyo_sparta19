591cfb578bbb96abad3e856094be5991fee33c3c
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-08-27 12:52:32 +0900

    docs: CustomAuditorAware에 javadoc 추가

--- src/main/java/sparta/jeogiyo/global/entity/CustomAuditorAware.java
+++ src/main/java/sparta/jeogiyo/global/entity/CustomAuditorAware.java
@@ -6,6 +6,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import sparta.jeogiyo.domain.user.UserDetailsImpl;
 
+/**
+ * BaseTimeEntity 클래스에서  @CreatedBy와  @LastModifiedBy를
+ * 자동으로 감지하기 위해 사용되는 클래스입니다.
+ */
 @Component
 public class CustomAuditorAware implements AuditorAware<String> {
 
