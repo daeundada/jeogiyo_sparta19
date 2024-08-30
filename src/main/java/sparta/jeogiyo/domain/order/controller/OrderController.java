ff29e5f4c5908f964446597560155a2c6690e95e
de123456sdf <cdaeun95@gmail.com>
2024-08-30 14:14:05 +0900

    feature: order 생성 구현

--- src/main/java/sparta/jeogiyo/domain/order/controller/OrderController.java
+++ src/main/java/sparta/jeogiyo/domain/order/controller/OrderController.java
@@ -1,4 +1,29 @@
 package sparta.jeogiyo.domain.order.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sparta.jeogiyo.domain.order.service.OrderService;
+import sparta.jeogiyo.global.response.ApiResponse;
+
+@RequestMapping("/api/orders")
+@RestController
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/cart/{cartId}")
+    public ResponseEntity<ApiResponse<Void>> CreateOrder(@PathVariable("cartId") Long cartId) {
+
+        orderService.CreateOrder(cartId);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.of("주문 생성 성공",null));
+    }
+
 }
--- src/main/java/sparta/jeogiyo/domain/order/service/OrderService.java
+++ src/main/java/sparta/jeogiyo/domain/order/service/OrderService.java
@@ -1,4 +1,14 @@
 package sparta.jeogiyo.domain.order.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+    public void CreateOrder(Long cartId) {
+
+        
+
+    }
+
 }
