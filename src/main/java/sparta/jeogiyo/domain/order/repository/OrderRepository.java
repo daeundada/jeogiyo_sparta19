acc51d79bb75b5ab3ccad602993ffa2f8010b7fa
de123456sdf <cdaeun95@gmail.com>
2024-09-01 23:21:27 +0900

    feature: OrderRepository 구현

--- src/main/java/sparta/jeogiyo/domain/order/repository/OrderRepository.java
+++ src/main/java/sparta/jeogiyo/domain/order/repository/OrderRepository.java
@@ -1,4 +1,16 @@
 package sparta.jeogiyo.domain.order.repository;
 
-public class OrderRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import sparta.jeogiyo.domain.order.entity.Order;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Page<Order> findByUser_UserId(Long userId, Pageable pageable);
+
+    Optional<Order> findByOrderId (UUID orderId);
 }
