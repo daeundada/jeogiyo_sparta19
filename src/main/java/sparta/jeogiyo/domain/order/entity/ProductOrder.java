6af6dee73b91bf1d9872f9d5b703ad89914fc938
de123456sdf <cdaeun95@gmail.com>
2024-09-02 14:51:18 +0900

    fix: Getter 어노테이션 추가

--- src/main/java/sparta/jeogiyo/domain/order/entity/ProductOrder.java
+++ src/main/java/sparta/jeogiyo/domain/order/entity/ProductOrder.java
@@ -1,10 +1,13 @@
 package sparta.jeogiyo.domain.order.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import lombok.Setter;
 import sparta.jeogiyo.domain.product.entity.Product;
 
+
 @Table(name = "p_product_orders")
+@Getter
 @Entity
 public class ProductOrder {
 
